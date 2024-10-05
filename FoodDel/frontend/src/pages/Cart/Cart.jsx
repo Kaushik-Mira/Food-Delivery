07fefb05db58da59747686399014da8e11f27f7b
Kaushik879 <heyradhika399@gmail.com>
2024-10-05 08:44:14 +0530

    changes

--- FoodDel/frontend/src/pages/Cart/Cart.jsx
+++ FoodDel/frontend/src/pages/Cart/Cart.jsx
@@ -1,11 +1,44 @@
-import './Cart.css';
+import { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
   return (
-    <div>
-      
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Item</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <>
+                <div className="cart-items-title cart-items-item" key={index}>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p className="cross" onClick={() => removeFromCart(item._id)}>
+                    X
+                  </p>
+                </div>
+                <hr />
+              </>
+            );
+          }
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
